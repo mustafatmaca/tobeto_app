42068e42c9f2c068ae60975a66063117f2f972cb
Elif Özcan <ozcann159@gmail.com>
2023-12-14 22:15:02 +0300

    theme added

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -9,17 +9,26 @@ class AppTheme {
       colorScheme: AppColor.colorScheme,
       primaryColor: AppColor.colorScheme.primary,
       scaffoldBackgroundColor: AppColor.colorScheme.background,
-      textTheme: GoogleFonts.ralewayTextTheme(),
+      textTheme: GoogleFonts.ralewayTextTheme().apply(
+        bodyColor: AppColor.textColor,
+      ),
     );
   }
 
   static ThemeData get darkTheme {
     return ThemeData(
-      useMaterial3: true,
-      colorScheme: AppColor.colorScheme,
-      primaryColor: AppColor.colorScheme.primary,
-      scaffoldBackgroundColor: AppColor.backgroundDark,
-      textTheme: GoogleFonts.ralewayTextTheme(),
-    );
+        useMaterial3: true,
+        colorScheme: AppColor.colorScheme,
+        primaryColor: AppColor.colorScheme.primary,
+        scaffoldBackgroundColor: AppColor.backgroundDark,
+        textTheme: GoogleFonts.ralewayTextTheme()
+            .apply(bodyColor: AppColor.textColorDark),
+        appBarTheme: AppBarTheme(
+            backgroundColor: AppColor.backgroundDark,
+            foregroundColor: AppColor.textColorDark),
+        drawerTheme: DrawerThemeData(backgroundColor: AppColor.backgroundDark),
+        iconButtonTheme: const IconButtonThemeData(),
+        iconTheme: IconThemeData(color: AppColor.textColorDark)
+        );
   }
 }
