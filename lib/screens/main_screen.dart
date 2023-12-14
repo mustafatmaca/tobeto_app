a7fe1edfe8fbfb4f424926b487504d6b4c10d615
Elif Özcan <ozcann159@gmail.com>
2023-12-14 18:49:18 +0300

    update

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -19,7 +19,7 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Tobeto"),
+          title: const Text("Tobeto"),
         ),
         drawer: Drawer(
           child: ListView(
