988c796cd7f351fd860a6261cb97200860bf6044
Elif Özcan <ozcann159@gmail.com>
2024-01-02 16:40:20 +0300

    catalog düzenleme

--- lib/screens/catalog_screen.dart
+++ lib/screens/catalog_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tobeto_app/widgets/custom_widgets/footer.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CatalogScreen extends StatefulWidget {
   const CatalogScreen({Key? key}) : super(key: key);
@@ -9,14 +10,57 @@ class CatalogScreen extends StatefulWidget {
 }
 
 class _CatalogScreenState extends State<CatalogScreen> {
+  Image img = Image.asset("assets/edu-banner2.png");
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Center(
-          child: Text("Catalog"),
+        ListView(
+          children: [
+            Column(
+              children: [
+                Container(
+                    height: MediaQuery.of(context).size.height * 0.3,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            fit: BoxFit.fill, image: img.image)),
+                    child: Container(
+                      decoration:
+                          BoxDecoration(color: Colors.black.withOpacity(0.3)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Öğrenmeye Başla !",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 44.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            
+                            TextField(
+                                decoration: InputDecoration(
+                                    fillColor: Colors.grey,
+                                    filled: true,
+                                    suffixIcon: const Icon(Icons.search),
+                                    label: const Text("Parola"),
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    contentPadding: const EdgeInsets.all(8))),
+                          ],
+                        ),
+                      ),
+                    )),
+                Row(
+                  children: [],
+                ),
+                Footer(),
+              ],
+            ),
+          ],
         ),
-        Footer(),
       ],
     );
   }
