5bc3965b3ec16e270ec5b3386c3a3fac2381901d
oguzhancotur <oguzhancotur55@gmail.com>
2024-01-22 16:05:28 +0300

    firebase add.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tobeto_app/firebase_options.dart';
 import 'package:tobeto_app/screens/splash/splash_screen.dart';
 import 'package:tobeto_app/theme/app_theme.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
