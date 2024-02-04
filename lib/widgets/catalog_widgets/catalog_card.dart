f55d712d63711d662664f6cd91335ee52bc8fadc
oguzhancotur <oguzhancotur55@gmail.com>
2024-02-05 00:01:43 +0300

    catalog and video display pages have been modified and updated.

--- lib/widgets/catalog_widgets/catalog_card.dart
+++ lib/widgets/catalog_widgets/catalog_card.dart
@@ -5,96 +5,105 @@ class CatalogCard extends StatelessWidget {
   final String image;
   final String instructor;
   final int time;
+  final bool isVisible;
 
-  CatalogCard({
-    super.key,
+  const CatalogCard({
+    Key? key,
     required this.title,
     required this.image,
     required this.instructor,
     required this.time,
-  });
+    required this.isVisible,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(15.0),
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: [
-          Image.asset(
-            image,
-            fit: BoxFit.cover,
-          ),
-          Container(
-            alignment: Alignment.bottomLeft,
-            padding: const EdgeInsets.all(8.0),
-            decoration: const BoxDecoration(
-                color: Color.fromARGB(255, 106, 92, 92),
-                borderRadius: BorderRadius.horizontal(
-                    right: Radius.circular(19), left: Radius.circular(19))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      instructor,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 13.0,
-                      ),
-                    ),
-                    SizedBox(
-                      width: 2,
-                    ),
-                    Icon(
-                      Icons.access_time,
-                      size: 18,
-                      color: Colors.white,
-                    ),
-                    SizedBox(
-                      width: 3,
-                    ),
-                    Text(
-                      (time / 60).toInt().toString() +
-                          " Saat"
-                              " " +
-                          (time % 60).toString() +
-                          " Dakika",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14.0,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  title,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  maxLines: 2,
-                ),
-              ],
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 15, bottom: 15),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20.0),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Image.asset(
+              image,
+              fit: BoxFit.cover,
             ),
-          ),
-          const Positioned(
-            right: 12,
-            top: 10,
-            child: CircleAvatar(
-              backgroundColor: Colors.deepPurple,
-              child: Icon(
-                Icons.play_arrow,
-                color: Colors.white,
+            Container(
+              alignment: Alignment.bottomLeft,
+              padding: const EdgeInsets.all(8.0),
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Color(0xFF004D79),
+                    Color(0xFF011D42),
+                    Color(0xFF341132),
+                  ], begin: Alignment.topCenter, end: Alignment.bottomLeft),
+                  borderRadius: BorderRadius.horizontal(
+                      right: Radius.circular(19), left: Radius.circular(19))),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        instructor,
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.background,
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      Icon(
+                        Icons.access_time,
+                        size: 18,
+                        color: Theme.of(context).colorScheme.background,
+                      ),
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        (time / 60).toInt().toString() +
+                            " Saat"
+                                " " +
+                            (time % 60).toString() +
+                            " Dakika",
+                        style: TextStyle(
+                            color: Theme.of(context).colorScheme.background,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    title,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.background,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                  ),
+                ],
               ),
             ),
-          )
-        ],
+            Positioned(
+              right: 12,
+              top: 10,
+              child: CircleAvatar(
+                backgroundColor: Color(0xFF011D42),
+                child: Icon(
+                  Icons.play_arrow,
+                  color: Theme.of(context).colorScheme.background,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/video_player_page.dart
+++ lib/widgets/video_player_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerPage extends StatefulWidget {
@@ -16,19 +17,22 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
       VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
 
   String _durationString = '';
+  bool _isPlaying = false;
+  bool _isFullScreen = false;
+  bool _showControls = false;
 
   @override
   void initState() {
     super.initState();
     _controller
-      ..addListener(_videoListener) // silindiğinde loopda kalıyor.
-      ..setLooping(false) //video bittiğinde tekrarlanmamasını sağlar.
+      ..addListener(_videoListener)
+      ..setLooping(false)
       ..initialize();
   }
 
   void _videoListener() {
-    if (!mounted)
-      return; // Eğer widget artık ekran üzerinde değilse güncelleme yapma
+    if (!mounted) return;
+
     setState(() {
       _durationString = _getDurationString();
     });
@@ -42,94 +46,128 @@ class _VideoPlayerPageState extends State<VideoPlayerPage> {
         : '${position.inMinutes}:${(position.inSeconds % 60).toString().padLeft(2, '0')} / ${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
   }
 
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void _togglePlayPause() {
+    setState(() {
+      if (_controller.value.isPlaying) {
+        _controller.pause();
+      } else {
+        _controller.play();
+      }
+      _isPlaying = _controller.value.isPlaying;
+    });
+  }
+
+  void _toggleFullScreen() {
+    setState(() {
+      _isFullScreen = !_isFullScreen;
+    });
+
+    if (_isFullScreen) {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.landscapeLeft,
+        DeviceOrientation.landscapeRight,
+      ]);
+    } else {
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: SystemUiOverlay.values);
+      SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+        DeviceOrientation.portraitDown,
+      ]);
+    }
+  }
+
+  void _toggleControlsVisibility() {
+    setState(() {
+      _showControls = !_showControls;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return Center(
-            child: _controller.value.isInitialized
-                ? Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      AspectRatio(
-                        aspectRatio: _controller.value.aspectRatio,
-                        child: VideoPlayer(_controller),
+        appBar: _isFullScreen
+            ? null
+            : AppBar(
+                title: Text(widget.title),
+              ),
+        body: GestureDetector(
+          onTap: _toggleControlsVisibility,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/arkavideo.png'), // Arka plan resminin yolu
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Center(
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    child: VideoPlayer(_controller),
+                  ),
+                  if (_showControls)
+                    Container(
+                      color: Colors.black54,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.replay_10, color: Colors.white),
+                            onPressed: () {
+                              _controller.seekTo(
+                                Duration(
+                                  seconds:
+                                      _controller.value.position.inSeconds - 10,
+                                ),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              _isPlaying ? Icons.pause : Icons.play_arrow,
+                              color: Colors.white,
+                            ),
+                            onPressed: _togglePlayPause,
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.forward_10, color: Colors.white),
+                            onPressed: () {
+                              _controller.seekTo(
+                                Duration(
+                                  seconds:
+                                      _controller.value.position.inSeconds + 10,
+                                ),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              _isFullScreen
+                                  ? Icons.fullscreen_exit
+                                  : Icons.fullscreen,
+                              color: Colors.white,
+                            ),
+                            onPressed: _toggleFullScreen,
+                          ),
+                        ],
                       ),
-                      SizedBox(height: 16.0),
-                      Text(_durationString),
-                      VideoProgressIndicator(
-                        _controller,
-                        allowScrubbing: true,
-                        colors: VideoProgressColors(
-                          playedColor: Color(0xFF341132),
-                          bufferedColor: Colors.white,
-                          backgroundColor: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  )
-                : CircularProgressIndicator(),
-          );
-        },
-      ),
-      bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 4.0,
-        child: Container(
-          height: 56.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              IconButton(
-                icon: Icon(Icons.replay_10),
-                onPressed: () {
-                  _controller.seekTo(
-                    Duration(
-                      seconds: _controller.value.position.inSeconds - 10,
                     ),
-                  );
-                },
+                ],
               ),
-              IconButton(
-                icon: Icon(
-                  _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-                ),
-                onPressed: () {
-                  setState(() {
-                    if (_controller.value.isPlaying) {
-                      _controller.pause();
-                    } else {
-                      _controller.play();
-                    }
-                  });
-                },
-              ),
-              IconButton(
-                icon: Icon(Icons.forward_10),
-                onPressed: () {
-                  _controller.seekTo(
-                    Duration(
-                      seconds: _controller.value.position.inSeconds + 10,
-                    ),
-                  );
-                },
-              ),
-            ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
