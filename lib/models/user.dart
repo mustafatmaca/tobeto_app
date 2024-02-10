30ef97cd86d5cb9bdf481f5db12d52c91d6ffbb6
oguzhancotur <oguzhancotur55@gmail.com>
2024-02-10 15:33:48 +0300

    changes

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
@@ -25,4 +27,58 @@ class User {
       this.certificates,
       this.languages,
       this.socials});
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'name': name});
+    result.addAll({'surname': surname});
+    result.addAll({'email': email});
+    if (gsm != null) {
+      result.addAll({'gsm': gsm});
+    }
+    if (birthdate != null) {
+      result.addAll({'birthdate': birthdate!.toMap()});
+    }
+    if (adress != null) {
+      result.addAll({'adress': adress});
+    }
+    if (about != null) {
+      result.addAll({'about': about});
+    }
+    if (skills != null) {
+      result.addAll({'skills': skills});
+    }
+    if (certificates != null) {
+      result.addAll({'certificates': certificates});
+    }
+    if (languages != null) {
+      result.addAll({'languages': languages});
+    }
+    if (socials != null) {
+      result.addAll({'socials': socials});
+    }
+
+    return result;
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    return User(
+      map['name'] ?? '',
+      map['surname'] ?? '',
+      map['email'] ?? '',
+      map['gsm'],
+      map['birthdate'] != null ? Timestamp.fromMap(map['birthdate']) : null,
+      map['adress'],
+      map['about'],
+      List<String>.from(map['skills']),
+      List<String>.from(map['certificates']),
+      List<String>.from(map['languages']),
+      List<String>.from(map['socials']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
 }
