a853fc8b89f121d8f0c60bb916afa820e2618a0b
oguzhancotur <oguzhancotur55@gmail.com>
2024-02-16 17:53:01 +0300

    Video player page refactor.

--- lib/widgets/video_player_page.dart
+++ lib/widgets/video_player_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:lecle_yoyo_player/lecle_yoyo_player.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerPage extends StatefulWidget {
@@ -15,159 +15,101 @@ class VideoPlayerPage extends StatefulWidget {
 class _VideoPlayerPageState extends State<VideoPlayerPage> {
   late final VideoPlayerController _controller =
       VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
-
-  String _durationString = '';
-  bool _isPlaying = false;
-  bool _isFullScreen = false;
-  bool _showControls = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller
-      ..addListener(_videoListener)
-      ..setLooping(false)
-      ..initialize();
-  }
-
-  void _videoListener() {
-    if (!mounted) return;
-
-    setState(() {
-      _durationString = _getDurationString();
-    });
-  }
-
-  String _getDurationString() {
-    Duration position = _controller.value.position;
-    Duration duration = _controller.value.duration;
-    return (position == null || duration == null)
-        ? ''
-        : '${position.inMinutes}:${(position.inSeconds % 60).toString().padLeft(2, '0')} / ${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
-  }
-
-  void _togglePlayPause() {
-    setState(() {
-      if (_controller.value.isPlaying) {
-        _controller.pause();
-      } else {
-        _controller.play();
-      }
-      _isPlaying = _controller.value.isPlaying;
-    });
-  }
-
-  void _toggleFullScreen() {
-    setState(() {
-      _isFullScreen = !_isFullScreen;
-    });
-
-    if (_isFullScreen) {
-      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-          overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
-      SystemChrome.setPreferredOrientations([
-        DeviceOrientation.landscapeLeft,
-        DeviceOrientation.landscapeRight,
-      ]);
-    } else {
-      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-          overlays: SystemUiOverlay.values);
-      SystemChrome.setPreferredOrientations([
-        DeviceOrientation.portraitUp,
-        DeviceOrientation.portraitDown,
-      ]);
-    }
-  }
-
-  void _toggleControlsVisibility() {
-    setState(() {
-      _showControls = !_showControls;
-    });
-  }
+  bool fullscreen = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _isFullScreen
-            ? null
-            : AppBar(
-                title: Text(widget.title),
+      appBar: fullscreen == false
+          ? AppBar(
+              title: Text(widget.title),
+              centerTitle: true,
+              leading: IconButton(
+                icon: const Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-        body: GestureDetector(
-          onTap: _toggleControlsVisibility,
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                    'assets/arkavideo.png'), // Arka plan resminin yolu
-                fit: BoxFit.cover,
+            )
+          : null,
+      body: Padding(
+        padding: fullscreen
+            ? EdgeInsets.zero
+            : EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.0),
+        child: YoYoPlayer(
+          autoPlayVideoAfterInit: false,
+          aspectRatio: 16 / 9,
+          url: widget.videoUrl,
+          videoStyle: VideoStyle(
+            progressIndicatorColors: VideoProgressColors(
+                playedColor: Colors.purple,
+                backgroundColor: Colors.black.withOpacity(0.3)),
+            qualityStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                color: Theme.of(context).colorScheme.background,
+                fontWeight: FontWeight.bold),
+            forwardAndBackwardBtSize: MediaQuery.of(context).size.width * 0.05,
+            actionBarBgColor: Colors.black.withOpacity(0.3),
+            actionBarPadding: EdgeInsets.zero,
+            forwardIcon: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.035,
+              backgroundColor: Colors.black.withOpacity(0.3),
+              child: Icon(
+                Icons.forward_10,
+                color: Theme.of(context).colorScheme.background,
+                size: MediaQuery.of(context).size.width * 0.04,
               ),
             ),
-            child: Center(
-              child: Stack(
-                alignment: Alignment.bottomCenter,
+            backwardIcon: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.035,
+              backgroundColor: Colors.black.withOpacity(0.3),
+              child: Icon(
+                Icons.replay_10,
+                color: Theme.of(context).colorScheme.background,
+                size: MediaQuery.of(context).size.width * 0.05,
+              ),
+            ),
+            playIcon: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.045,
+              backgroundColor: Colors.black.withOpacity(0.3),
+              child: Icon(
+                Icons.play_arrow_rounded,
+                color: Theme.of(context).colorScheme.background,
+                size: MediaQuery.of(context).size.width * 0.08,
+              ),
+            ),
+            pauseIcon: CircleAvatar(
+              radius: MediaQuery.of(context).size.width * 0.045,
+              backgroundColor: Colors.black.withOpacity(0.3),
+              child: Icon(
+                Icons.pause,
+                color: Theme.of(context).colorScheme.background,
+                size: MediaQuery.of(context).size.width * 0.08,
+              ),
+            ),
+            videoQualityPadding: EdgeInsets.all(4.0),
+            // showLiveDirectButton: true,
+            // enableSystemOrientationsOverride: false,
+          ),
+          videoLoadingStyle: const VideoLoadingStyle(
+            loading: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  AspectRatio(
-                    aspectRatio: _controller.value.aspectRatio,
-                    child: VideoPlayer(_controller),
-                  ),
-                  if (_showControls)
-                    Container(
-                      color: Colors.black54,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          IconButton(
-                            icon: Icon(Icons.replay_10, color: Colors.white),
-                            onPressed: () {
-                              _controller.seekTo(
-                                Duration(
-                                  seconds:
-                                      _controller.value.position.inSeconds - 10,
-                                ),
-                              );
-                            },
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              _isPlaying ? Icons.pause : Icons.play_arrow,
-                              color: Colors.white,
-                            ),
-                            onPressed: _togglePlayPause,
-                          ),
-                          IconButton(
-                            icon: Icon(Icons.forward_10, color: Colors.white),
-                            onPressed: () {
-                              _controller.seekTo(
-                                Duration(
-                                  seconds:
-                                      _controller.value.position.inSeconds + 10,
-                                ),
-                              );
-                            },
-                          ),
-                          IconButton(
-                            icon: Icon(
-                              _isFullScreen
-                                  ? Icons.fullscreen_exit
-                                  : Icons.fullscreen,
-                              color: Colors.white,
-                            ),
-                            onPressed: _toggleFullScreen,
-                          ),
-                        ],
-                      ),
-                    ),
+                  Text("Loading video..."),
                 ],
               ),
             ),
           ),
-        ));
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+          onFullScreen: (value) {
+            setState(() {
+              if (fullscreen != value) {
+                fullscreen = value;
+              }
+            });
+          },
+        ),
+      ),
+    );
   }
 }
